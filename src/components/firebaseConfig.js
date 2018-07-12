6103aa0f0e3ced6c73fccddf87a34b9dd52214e4
Hayley <32654472+hayleyas@users.noreply.github.com>
2018-07-11 21:40:12 -0300

    Add files via upload

---
+++ src/components/firebaseConfig.js
@@ -0,0 +1,8 @@
+export default {
+    apiKey: "AIzaSyDS6auxKqRxgN6gTxKmbSvIvQamasPK1xA",
+    authDomain: "vuewfs-prod-36fb3.firebaseapp.com",
+    databaseURL: "https://vuewfs-prod-36fb3.firebaseio.com",
+    projectId: "vuewfs-prod-36fb3",
+    storageBucket: "vuewfs-prod-36fb3.appspot.com",
+    messagingSenderId: "145738786024"
+}
